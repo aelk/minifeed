58bb8b448a2824bf147e62239056dceb60d27d65
Andrew Elkouri <andrewelkouri@gmail.com>
2020-12-18 20:49:57 -0500

    flask setup + feed selection

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from flask import Flask, request, jsonify, render_template
 import feedparser
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    feedList = ['https://justthenews.com/rss.xml', 'https://nypost.com/feed/']
+    if request.method == 'POST':
+        print(request.form.getlist('feedname'))
+
+    return render_template('index.html')
+
+@app.route('/feed')
+def load_feed(feed_list):
     feeds = map(feedparser.parse, feedList)
+    titles = []
     for feed in feeds:
         for entry in feed['entries']:
-            print(entry['title'])
+            titles.append(entry['title'])
+    return jsonify(titles)
 
 if __name__ == '__main__':
-    main()
+    app.run()
